cc8a83a059f5c2f0e7b2b9ddc47265f29dcd06b6
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-01 22:12:52 +0200

    fine prima parte

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server', // una stringa, a piacimento, unica, senza la possibilità di poterla confondere.
-    template: 'SERVER WORKS', // file html di riferimento del componente 
+    templateUrl: './server.component.html', // file html di riferimento del componente 
 })
 export class ServerComponent {
 

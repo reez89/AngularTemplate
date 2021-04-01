4534660e57ec67902d7632b5a09b679b9bcdc88b
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-01 21:46:29 +0200

    aggiunto componente tramite cli

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server', // una stringa, a piacimento, unica, senza la possibilità di poterla confondere.
+    template: 'SERVER WORKS', // file html di riferimento del componente 
+})
+export class ServerComponent {
+
+
+
+}

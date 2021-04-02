aad3b9484383172782b30c29fbbd0ca1b6d2ce29
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-03 00:54:19 +0200

    ngIf & ngElse

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server', // una stringa, a piacimento, unica, senza la possibilità di poterla confondere.
-    templateUrl: './server.component.html', // file html di riferimento del componente 
+  selector: 'app-server', // una stringa, a piacimento, unica, senza la possibilità di poterla confondere.
+  templateUrl: './server.component.html', // file html di riferimento del componente
 })
 export class ServerComponent {
-    serverId: number =  10;
-    serverStatus: string = ' offline';
+  serverId: number = 10;
+  serverStatus: string = ' offline';
 
-    getServerSatus(){
-        return this.serverStatus;
-    }
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+  getServerSatus() {
+    return this.serverStatus;
+  }
+
+  getCOlor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }

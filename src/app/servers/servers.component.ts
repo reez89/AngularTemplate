768b3c158018f5e6f32d39f1ad5ce47aa0a4de85
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-03 00:45:38 +0200

    ngIf

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,28 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus ='No server was created'
-  serverName = '' ;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
+  serverCreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
-   }
-
-  ngOnInit(): void {
   }
 
-  onCreateServer(){
-    this.serverCreationStatus ='Server was created!' + "Name is " + this.serverName;
-    this.serverName = '';
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus =
+      'Server was created!' + 'Name is ' + this.serverName;
   }
 
-  onupdateServerName(event: Event){
+  onupdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value; // HTMLInputElement serve a far capire che tipo di evento si sta passando.
-    
   }
 }
